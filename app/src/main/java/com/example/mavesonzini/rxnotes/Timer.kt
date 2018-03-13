e9cce3506795da1f5bd5b8fc9b3a831b950b6a3a
Veronica Sonzini <mavesonzini@gmail.com>
2018-03-13 19:07:27 +0000

    First commit. Assets. Benchmarks Activity, Weightlifts Activity, Timers Activity

---
+++ app/src/main/java/com/example/mavesonzini/rxnotes/Timer.kt
@@ -0,0 +1,12 @@
+package com.example.mavesonzini.rxnotes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Timer : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+    }
+}
